54d73dd51921677103d9aef4087daada0a8f16b3
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-05-13 10:44:51 +0700

    đề trang 12 câu 4 trong tổng hợp đề thi của hệ quản trị cơ sở dữ liệu

---
+++ SQLQuery5.sql
@@ -0,0 +1,64 @@
+﻿-- cau 4
+	-- a
+if exists(select * from sys.objects where name = 'func_TKeDuAn')
+	drop function func_TKeDuAn;
+go
+
+create function func_TKeDuAn(@fromYear int, @toYear int)
+returns table
+as 
+return
+(
+	select	year(d.NgayBatDau) as [Year],
+			count(d.MaDuAn) as 'So Luong'
+	from DuAn as d
+	where year(d.NgayBatDau) between @fromYear and @toYear
+	group by year(d.NgayBatDau)
+)
+go
+
+-- testcase
+select * from dbo.func_TKeDuAn(2020, 2023)
+
+-- b
+if exists(select * from sys.objects where name = 'func_TKeDuAn_DayDuCacNam')
+	drop function func_TKeDuAn_DayDuCacNam;
+go
+
+create function func_TKeDuAn_DayDuCacNam(@fromYear int, @toYear int)
+returns @tbl table
+(
+	[Year] int primary key,
+	[SoLuong] int default(0)
+)
+as
+begin
+	declare @tblNam table
+	(
+		[Year] int
+	);
+	declare @y int = @fromYear;
+	while (@y < @toYear)
+		begin
+			insert into @tblNam values(@y);
+			set @y += 1;
+		end
+
+	insert into @tbl([Year], [SoLuong])
+		select t1.Year, isnull(t2.SoLuong, 0) as SoLuong
+		from	@tblNam as t1
+				left join
+				(
+					select	year(d.NgayBatDau) as [Year],
+							count(d.MaDuAn) as SoLuong
+					from DuAn as d
+					where year(d.NgayBatDau) between @fromYear and @toYear
+					group by YEAR(d.NgayBatDau)
+				) as t2 on t1.Year = t2.Year
+
+	return;
+end
+go
+
+-- test case
+select * from dbo.func_TKeDuAn_DayDuCacNam(2010, 2027)
---
+++ schema_data_query.sql
@@ -0,0 +1,48 @@
+﻿CREATE DATABASE MSSV_20T1020001;
+GO
+
+USE MSSV_20T1020001;
+GO
+
+CREATE TABLE NhanVien (
+    MaNhanVien NVARCHAR(50) NOT NULL PRIMARY KEY,
+    HoTen NVARCHAR(50) NOT NULL,
+    NgaySinh DATE NOT NULL,
+    Email NVARCHAR(50) NOT NULL,
+    DiDong NVARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE DuAn (
+    MaDuAn NVARCHAR(50) NOT NULL PRIMARY KEY,
+    TenDuAn NVARCHAR(255) NOT NULL,
+    NgayBatDau DATE NOT NULL,
+    SoNguoiThamGia INT NOT NULL
+);
+GO
+
+CREATE TABLE NhanVien_DuAn (
+    MaNhanVien NVARCHAR(50) NOT NULL,
+    MaDuAn NVARCHAR(50) NOT NULL,
+    NgayGiaoViec DATE NOT NULL,
+    MoTaCongViec NVARCHAR(255),
+    PRIMARY KEY (MaNhanVien, MaDuAn),
+    FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien),
+    FOREIGN KEY (MaDuAn) REFERENCES DuAn(MaDuAn)
+);
+GO
+
+INSERT INTO NhanVien (MaNhanVien, HoTen, NgaySinh, Email, DiDong)
+VALUES
+('NV001', N'Nguyễn Thanh An', '1980-12-01', 'thanhan@gmail.com', '0914422578'),
+('NV002', N'Trần Chí Hiếu', '1985-05-17', 'hieu85@gmail.com', '0987454125'),
+('NV003', N'Vũ Thành Chung', '1986-11-20', 'chungvt@gmail.com', '0935254771'),
+('NV005', N'Lê Thị Hải Yến', '1986-08-14', 'lthyen@gmail.com', '0983120547');
+GO
+
+INSERT INTO DuAn (MaDuAn, TenDuAn, NgayBatDau, SoNguoiThamGia)
+VALUES
+('DA001', 'SmartUni', '2022-01-01', 6),
+('DA002', 'E-Shop', '2022-05-01', 5),
+('DA003', 'LiteCMS', '2022-09-01', 4);
+GO
